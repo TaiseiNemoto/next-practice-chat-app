9c448677fa1ff8e050e334168f57f6cdba772442
TaiseiNemoto <taisei.sn1022@gmail.com>
2023-03-06 23:09:39 +0900

    prettier eslint セットアップ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,7 @@ import Image from 'next/image'
 import type { NextPage } from 'next'
 
 const Page: NextPage = () => {
-  return (
-    <div>home</div>
-  )
+  return <div>home</div>
 }
 
 export default Page
