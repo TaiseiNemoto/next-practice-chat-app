5532ac997889a604334853137f2d6dbd25cd186a
TaiseiNemoto <nemoto@defide-ix.com>
2023-03-10 12:36:14 +0900

    [add]サインアップページの開発

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,32 +1,95 @@
-import { Container, Heading, chakra, Spacer, Grid, Box, FormControl, FormLabel, Input, Center, Button } from '@chakra-ui/react';
-import { useState } from 'react';
+import {
+  Container,
+  Heading,
+  chakra,
+  Spacer,
+  Grid,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Center,
+  Button,
+  useToast
+} from '@chakra-ui/react'
+import { FormEvent, useState } from 'react'
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+} from 'firebase/auth'
+import { FirebaseError } from '@firebase/util'
 
 export const Page = () => {
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const toast = useToast() 
 
-  const handleSubmit = () => {
-    console.log({ email, password });
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    setIsLoading(true)
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      await sendEmailVerification(userCredential.user)
+      setEmail('')
+      setPassword('')
+      toast({
+        title: '確認メールを送信しました。',
+        status: 'success',
+        position: 'top'
+      })
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        toast({
+          title: 'エラーが発生しました。',
+          status: 'error',
+          position: 'top'
+        })
+        console.log(e)
+      }
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   return (
     <Container py={14}>
       <Heading>サインアップ</Heading>
-      <chakra.form  onSubmit={handleSubmit}>
-        <Spacer  height={8} aria-hidden />
+      <chakra.form onSubmit={handleSubmit}>
+        <Spacer height={8} aria-hidden />
         <Grid gap={4}>
           <Box display={'contents'}>
             <FormControl>
               <FormLabel>メールアドレス</FormLabel>
-              <Input type={'email'} />
+              <Input
+                type={'email'}
+                name={'email'}
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value)
+                }}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>パスワード</FormLabel>
-              <Input type={'password'} />
+              <Input
+                type={'password'}
+                name={'password'}
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                }}
+              />
             </FormControl>
           </Box>
         </Grid>
-        <Spacer  height={4} aria-hidden />
+        <Spacer height={4} aria-hidden />
         <Center>
           <Button type={'submit'}>アカウントを作成</Button>
         </Center>
