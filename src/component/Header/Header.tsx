100069a98fe681637c8ee00d051644fd13860f8c
TaiseiNemoto <nemoto@defide-ix.com>
2023-03-15 16:18:20 +0900

    [add]サインアウト機能実装

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,14 +1,43 @@
-import { chakra, Container, Heading } from "@chakra-ui/react"
+import { chakra, Container, Heading, Button, useToast } from "@chakra-ui/react"
 import { useAuthContext } from '../../feature/auth/provider/AuthProvider';
+import { getAuth, signOut } from '@firebase/auth';
+import { FirebaseError } from '@firebase/util';
+import { useState } from 'react';
+import { useRouter } from "next/router";
 
 export const Header = () => {
   const { user } = useAuthContext()
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const toast = useToast()
+  const { push } = useRouter()
+
+  const handleSignOut = async () => {
+    setIsLoading(true)
+    try {
+      const auth = getAuth()
+      await signOut(auth)
+      toast({
+        title: 'ログアウトしました。',
+        status: 'success',
+        position: 'top'
+      })
+      push('/signin')
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        console.log(e)
+      } 
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return (
     <chakra.header py={4} bgColor={'blue.600'}>
       <Container maxW={'container.lg'}>
         <Heading color={'white'}>
-          {user ? 'ログイン中' : 'ログアウト'}
+          {user ? (
+            <Button colorScheme={'teal'} onClick={handleSignOut} isLoading={isLoading}>サインアウト</Button>
+          ) : 'ログアウト'}
         </Heading>
       </Container>
     </chakra.header>
